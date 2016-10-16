02eba41d92f7cf49722f3220d52120c94df2c6c1
Rachel Soma <rachel@rachelsoma.com>
2016-10-16 15:10:24 +1100

    Sorting by Name and Genre ✔️

--- src/MovieApp.java
+++ src/MovieApp.java
@@ -7,12 +7,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.io.File;
-// import java.io.FileNotFoundException;
 import java.io.IOException;
-// import java.io.PrintWriter;
-// import java.util.Date;
-// import java.text.ParseException;
-// import java.text.SimpleDateFormat;
+
 
 public class MovieApp {
   static Scanner kb = new Scanner(System.in);
@@ -24,6 +20,7 @@ public class MovieApp {
     int returnMenu;
     int returnMovieMenu;
     int returnPlaylistMenu;
+    int returnSortMenu;
     Movie[] arrayMovies = null;
     Playlist[] arrayPlaylists = null;
     String fileNameMovies = "movieLibrary.txt";
@@ -48,7 +45,6 @@ public class MovieApp {
 
         returnMovieMenu = displayMenu(movieMenuArray);
 
-
         if (returnMovieMenu == 1) {
 
           System.out.println("****Display Movies****");
@@ -56,11 +52,20 @@ public class MovieApp {
 
           returnMovieMenu = displayMenu(movieMenuArray);
         }
-        if (returnMovieMenu == 2) {
+        if (returnMovieMenu == 2) {         
           System.out.println("****Sort Movies****");
-          selectionSort(arrayMovies);
-          printMovies(arrayMovies);
-
+          
+          
+          String[] sortMenuArray = {"Sort by Name", "Sort by Genre","Exit menu"};
+          returnSortMenu = displayMenu(sortMenuArray);
+          if (returnSortMenu == 1){
+            sortName(arrayMovies);
+          } else if (returnSortMenu ==  2){
+            sortGenre(arrayMovies);
+          }
+          
+          
+          
           returnMovieMenu = displayMenu(movieMenuArray);
         }
         if (returnMovieMenu == 3) {
@@ -109,7 +114,7 @@ public class MovieApp {
         break;
     }
 
-    System.out.println("Total: " + countMovies); // print total movies
+    //System.out.println("Total: " + countMovies); // print total movies
 
 
     kb.close();
@@ -149,7 +154,6 @@ public class MovieApp {
 
   static void fileExists(File inFile, String fileName) throws IOException {
 
-    // Scanner kb = new Scanner(System.in);
     while (!inFile.exists()) { // validates that file exists
 
       System.out.println("File not found"); // alerts user to missing file
@@ -173,9 +177,6 @@ public class MovieApp {
     while (inputFile.hasNextLine() && countMovies < arrayMovies.length) {
       str = inputFile.nextLine();
       tokens = str.split(",");
-      // for (int i =0; i < tokens.length; i++){ //loop through tokens
-      // System.out.print(tokens[i] + " "); //print one token
-      // }
 
       // parsing int/double inspiration from
       // http://stackoverflow.com/questions/8348591/splitting-string-and-put-it-on-int-array
@@ -227,23 +228,13 @@ public class MovieApp {
     int playlistID; // : a unique numeric identifier for the playlist
     String name; // : a name for the playlist
     int length;// : the number of movies in the playlist
-    // StringBuilder movies; // : a list of zero or more Movie IDs which make up this playlist
-    // int[] array;
+
 
     Playlist[] arrayPlaylists = new Playlist[50];
     // Read each line, split, then print
     while (inputFile.hasNextLine() && countPlaylists < arrayPlaylists.length) {
       str = inputFile.nextLine();
 
-      // int playlistID = Integer.parseInt(tokens[0]);// : a unique numeric identifier for the
-      // playlist
-      // String name = tokens[1];// : a name for the playlist
-      // int length = Integer.parseInt(tokens[2]);// : the number of movies in the playlist
-      //
-      // for (i=3; tokens.hasMoreTokens(); i++)
-      // movies = tokens[i];
-      // //movies = tokens[3];
-      //
 
       // creating string tokenizer
       StringTokenizer tokenizer = new StringTokenizer(str, ",");
@@ -284,27 +275,55 @@ public class MovieApp {
     }
   }
 
-  public static void selectionSort(Movie[] arr) {
+  public static void sortName(Movie[] arr) {
     int startAt; // index of starting position
     int j; // loop control
     int minIndex; // index of smallest element
     Movie tempObj; // temp object for swapping
     for (startAt = 0; startAt < arr.length - 1; startAt++) {
-        minIndex = startAt; // assume smallest is at the start of arr
-        tempObj = arr[startAt];
-        if (arr[startAt] != null) {
+      minIndex = startAt; // assume smallest is at the start of arr
+      tempObj = arr[startAt];
+      if (arr[startAt] != null) {
         // look through the rest of the array for value smaller than starting value
-           
-        for (j = startAt + 1; j < arr.length  && arr[j] != null; j++) {
+
+        for (j = startAt + 1; j < arr.length && arr[j] != null; j++) {
           if (arr[j].getMovieTitle().compareTo(tempObj.getMovieTitle()) < 0) { // compare current
                                                                                // element with
                                                                                // tempObj
+            tempObj = arr[j]; // set temp to be the new smallest value
             minIndex = j; // record where that smallest value is located
           }
         }
         arr[minIndex] = arr[startAt]; // perform the swap
         arr[startAt] = tempObj; // complete the swap
-        }
+      }
     }
+    printMovies(arr);
+  }
+  
+  public static void sortGenre(Movie[] arr) {
+    int startAt; // index of starting position
+    int j; // loop control
+    int minIndex; // index of smallest element
+    Movie tempObj; // temp object for swapping
+    for (startAt = 0; startAt < arr.length - 1; startAt++) {
+      minIndex = startAt; // assume smallest is at the start of arr
+      tempObj = arr[startAt];
+      if (arr[startAt] != null) {
+        // look through the rest of the array for value smaller than starting value
+
+        for (j = startAt + 1; j < arr.length && arr[j] != null; j++) {
+          if (arr[j].getGenre().compareTo(tempObj.getGenre()) < 0) { // compare current
+                                                                               // element with
+                                                                               // tempObj
+            tempObj = arr[j]; // set temp to be the new smallest value
+            minIndex = j; // record where that smallest value is located
+          }
+        }
+        arr[minIndex] = arr[startAt]; // perform the swap
+        arr[startAt] = tempObj; // complete the swap
+      }
+    }
+    printMovies(arr);
   }
 }
